2c0bb64e6fc98e47b717f1338e5e3a16f8dab23e
Joe Burgess <joe@flatironschool.com>
2015-12-27 12:48:05 -0500

    adding oslution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :show]
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
