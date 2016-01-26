2d0f2821be044777218f7becf94abe828f7e55f4
John Anthony Rivera <askinganthony@gmail.com>
2016-01-26 10:39:21 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,9 +3,19 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+    render 'students/index'
   end
 
   def show
+    @student = Student.find(params[:id])
+    render 'students/show'
+  end
+
+  def activate
+      set_student
+      @student.active=!(@student.active)
+      @student.save
+      redirect_to student_url(@student)
   end
 
   private
@@ -13,4 +23,6 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
+
 end
