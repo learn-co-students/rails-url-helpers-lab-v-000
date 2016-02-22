ee53906ee2cb43b75095f4a8e5e64ff7fe068d73
jaeku <ja.ku@rutgers.edu>
2016-02-22 01:31:16 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    set_student
+    @student.active = !@student.active
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
   def to_s
-    self.first_name + " " + self.last_name
+    "#{self.first_name} #{self.last_name}"
   end
 end
