331c53463ea61bf17d4a851f4bf6b0f2d1285434
Chris Bruen <bruen.chris@gmail.com>
2016-03-03 13:54:14 -0500

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 class Student < ActiveRecord::Base
+ 
   def to_s
     self.first_name + " " + self.last_name
   end
+  def active?
+  	if self.active
+  		return "This student is currently active."
+  	else
+  		return "This student is currently inactive."
+  	end
+  end	
 end
--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,5 +1,6 @@
 require 'byebug'
 require 'rails_helper'
+require 'pry'
 
 describe 'Route to view' do
   it 'has an index page' do
@@ -12,7 +13,7 @@ describe 'Multiple students' do
   it 'shows them on the index page' do
     Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     Student.create!(first_name: "Lindsey", last_name: "Stirling")
-
+	
     visit students_path
     expect(page).to have_content(/Daenerys|Lindsey/)
   end
