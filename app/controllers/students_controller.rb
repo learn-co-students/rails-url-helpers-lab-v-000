eb965298c90ad47f54e582c905938b1734064140
marcotsui2003 <marcotsui2003@yahoo.com>
2016-03-03 14:00:42 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,8 +5,20 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
+  def show  
+    @student= Student.find(params[:id])
+    @student.active ? @status ="active" : @status="inactive"
   end
+
+  def activate
+    @student= Student.find(params[:id])
+    @student.active =!(!!@student.active)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+
+  
 
   private
 
