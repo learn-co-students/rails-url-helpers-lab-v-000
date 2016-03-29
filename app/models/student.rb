184b611d9a24e60fb2a7d41d94dbceeb276d7880
fiveinfinity <jlachance1@gmail.com>
2016-03-29 12:40:23 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,7 @@
 class Student < ActiveRecord::Base
-  attr_accessor :active
 
   def to_s
     self.first_name + " " + self.last_name
   end
-
-  # def active
-  #   @active ||= false
-  # end
 
 end
