a42902eda0765dc931b0df8424d4ebaec8873d9b
smithWEBtek <brad@smithwebtek.com>
2016-05-10 17:03:29 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'pry'
 
 group :development, :test do
   gem 'byebug'
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,13 @@
 class Student < ActiveRecord::Base
+  
+  # After initialization, set default values
+  after_initialize :set_default_values
+    
+  def set_default_values
+    # Only set if 'active' attribute IS NOT set
+    self.active ||= "false"
+  end 
+
   def to_s
     self.first_name + " " + self.last_name
   end
