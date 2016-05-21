449ad1a1ce2574e7aa15cde3c81a15084ea02fec
Michael Glaser <michaelcharlesglaser@gmail.com>
2016-05-21 13:47:47 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :show]
+  get '/student/:id/edit', to: 'students#edit', as: 'activate_student'
 
   # This show route can be refactored into the above resouces' method call above, like so:
   # resources :students, only: [:index, :show]
