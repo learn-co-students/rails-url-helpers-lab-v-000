b21b00371b4ff95101053aaeeb22b63b4aa0e439
eliyooy <ebshimon@gmail.com>
2016-05-23 20:54:58 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,14 @@
 class Student < ActiveRecord::Base
+
+  def activate_student
+    self.active ? self.active = false : self.active = true
+    self.save
+  end
+
+  def status
+    self.active ? "This student is currently active." : "This student is currently inactive."
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
