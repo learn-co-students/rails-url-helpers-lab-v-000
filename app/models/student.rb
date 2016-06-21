3be737c42237c21d37e1517db11f98100da58192
ldgarber <leah.garber88@gmail.com>
2016-06-21 13:11:16 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,34 @@
 class Student < ActiveRecord::Base
+  after_initialize :set_defaults
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active? 
+    !!self.active 
+  end
+
+  def activate!
+    self.active = true
+    self.save
+  end
+
+  def deactivate!
+    self.active = false
+    self.save
+  end
+
+  def toggle_active!
+    if self.active?
+      self.deactivate!
+    else
+      self.activate!
+    end
+  end
+
+  private
+  def set_defaults
+    self.active ||= false
   end
 end
