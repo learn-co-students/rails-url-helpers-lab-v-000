233741875485d2732ad4916d4ec1a0f6e4f2ce9a
wedesignstudios <deelden@gmail.com>
2016-07-12 22:37:25 -0500

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :edit]
   
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def edit
+    if @student.active == false
+      @student.active = true
+      @student.save
+    elsif @student.active == true
+      @student.active = false
+      @student.save        
+    end
+    redirect_to ("/students/#{params[:id]}")    
   end
 
   private
