cfc18df59dcf8a4e204ea4231bec716967e43af0
andrewstone8 <andrewstone8@gmail.com>
2016-07-18 15:35:47 +0900

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def toggle
+    self.active = !self.active
+  end
+  
 end
---
+++ db/migrate/002_add_column.rb
@@ -0,0 +1,7 @@
+class AddColumn < ActiveRecord::Migration
+
+  def change
+    add_column :students, :active, :boolean, :default => false
+  end
+
+end
