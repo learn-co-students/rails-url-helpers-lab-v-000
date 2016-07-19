69e67a795f6caaa44df6109859b2048b86fe3c18
ndaniele <n.daniele@gmail.com>
2016-07-19 15:53:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   # However for the sake of this lab and seeing
   # how you can pass params through the
   # route, we'll keep it explicit and refactor it later
-  get "students/:id", to: "students#show"
+  get "students/:id/activate", to: "students#activate", as: "activate_student"
+
 end
