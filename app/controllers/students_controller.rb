85ba76b4d8f78ae5f2a8406b6a8c5c53cd2bf002
miclp <michaelpeimani@gmail.com>
2016-08-04 16:28:19 -0700

    Add actions for toggle active status

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    # toggle active status
+    @student.toggle_active_status
+    @student.save
+    redirect_to action: "show", id: @student.id
+  end
+
   private
 
     def set_student
