96a8f79b2de27c3e9dc876a5db62baa48c48841d
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-18 19:42:14 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: :show # --A call back
+  # --we can pass in an only option, selecting which of the seven RESTful routes we care about. In this case, we only care about the show action.
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def active
   end
 
   private
