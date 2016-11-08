4e5aa211d3bf6f1ecea1aa5f7f92f6322ddb8996
Will Lynch <will.lynch91@gmail.com>
2016-11-07 22:07:04 -0500

    almost done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+      @student = set_student
+      @student.active = !@student.active
+      render 'show'
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,10 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def default_value
+          @student.active = false
   end
 end
