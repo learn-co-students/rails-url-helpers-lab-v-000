6ab44030e9ce72573b0261e1fb831b5632a876ef
rgylling <mikegylling@yahoo.com>
2016-11-27 15:23:22 -0800

    Done.

---
+++ db/migrate/20161127231533_add_active_student.rb
@@ -0,0 +1,5 @@
+class AddActiveStudent < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, :default => true
+  end
+end
---
+++ db/migrate/20161127232238_new_active_student.rb
@@ -0,0 +1,5 @@
+class NewActiveStudent < ActiveRecord::Migration
+  def change
+    change_column :students, :active, :boolean, :default => false
+  end
+end
