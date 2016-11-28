2bcc2ffe4fb36946de4a3b983ef9f3c48f83c509
Bruno Boehm <bruno.boehm@gmail.com>
2016-11-28 13:14:38 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate, :toggle_state]
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student.toggle!(:active)
+    redirect_to action: "show", id: params[:id]
+  end
+
+  def toggle_state
+    @student.toggle!(:active)
+    redirect_to action: "show", id: params[:id]
   end
 
   private
