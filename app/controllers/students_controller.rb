02d293f5654f3b05088082ac21ae684fb367541b
Ray Mulligan <rmullig2@optonline.net>
2016-12-16 20:38:44 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student = Student.find(params[:format])
+    @student.active = !@student.active
+    @student.save
+    redirect_to "/students/#{@student.id}"
   end
 
   private
