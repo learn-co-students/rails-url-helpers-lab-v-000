4a6abe9f4022393d3021833c2be975898ac80729
msosborne3 <msosborne@ymail.com>
2016-12-17 02:10:54 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,15 @@ class StudentsController < ApplicationController
   def show
   end
 
+  # Toggles the student's active status
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active # Sets the new active status as the opposite of what it is.
+    @student.save
+
+    redirect_to student_path(@student) # Redirects to the student show page
+  end
+
   private
 
     def set_student
