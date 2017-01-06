ec670902bc2c015fdfc68ee5f8d05f4115086e33
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-01-06 04:22:58 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
+require 'pry'
+
 class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def reload
+    self.active ? (self.active = false) : (self.active = true)
   end
 end
