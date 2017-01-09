fcadfbaa85e11271a1511a288246a952e99f298c
github username pianistps <github email address pianistps@yahoo.com>
2017-01-09 15:41:43 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,16 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate_student_path
+
   end
 
   private
