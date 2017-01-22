a3cd2ab095cf173d5138c8c14cff15701bf59295
Keegan Leitz <kjleitz@gmail.com>
2017-01-21 20:03:40 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active?
+    self.active
+  end
+
+  def toggle_active
+    self.active = !self.active
+    self.save
+  end
 end
