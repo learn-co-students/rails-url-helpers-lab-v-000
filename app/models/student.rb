87fcd21cbd4ae3ed482cc5d6a29f610d69d9a7ee
Elyse Klova <elyse.klova@gmail.com>
2017-03-01 11:30:21 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,8 +2,4 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-
-  def activate_student_path
-    self.active = !self.active
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :students, only: [:show]
+  resources :students, only: [:show, :index]
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get '/students', to: 'students#index', as: 'students'
-  get '/students/:id/activate', to: 'students#edit'
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
