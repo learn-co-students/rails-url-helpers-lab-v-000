cb0ac130ee6bfb9ab8fc22346a686bac4b3d7d75
John Schlesinger <jschles1@ramapo.edu>
2017-03-18 23:44:49 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 class Student < ActiveRecord::Base
+  after_initialize :init
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  private
+
+    def init
+      self.active ||= false
+    end
 end
