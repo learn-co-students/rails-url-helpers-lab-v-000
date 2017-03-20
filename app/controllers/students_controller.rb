11d15296ddfdd471e802f9af0249b2b51cd4839f
Alex Glover <Alex@MacBook-Pro-3.local>
2017-03-20 12:14:29 -0400

    All tests passing except 4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
   end
 
   private
