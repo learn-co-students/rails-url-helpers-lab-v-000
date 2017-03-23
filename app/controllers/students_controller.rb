66dcc6edab43dea5bbdd6aa02eaa55a59b1ce041
Hui Jiang <hui1021@yahoo.com>
2017-03-23 15:39:59 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
@@ -13,4 +21,5 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
 end
---
+++ db/migrate/003_change_active_column.rb
@@ -0,0 +1,5 @@
+class ChangeActiveColumn < ActiveRecord::Migration
+  def change
+    change_column :students, :active, :boolean, :default => false
+  end
+end
