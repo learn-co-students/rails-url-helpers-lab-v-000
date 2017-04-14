df487a45515df60a3b75bf80b5fbd750821221e7
Damien de Lartigue <damien@delartigue.co.uk>
2017-04-14 04:36:38 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_phrase
+    if active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
 end
