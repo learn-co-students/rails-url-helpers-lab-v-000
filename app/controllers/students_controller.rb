91c80f2e2c1b0f68ad7eb182abce295f5cdb10ef
Trai Compton <tlcompton2546@outlook.com>
2017-04-26 00:10:14 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,29 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find_by_id(params[:id])
+
+    if @student.active
+      @status = "active"
+    else
+      @status = "inactive"
+    end
+  end
+
+  def activate
+    @student = Student.find_by_id(params[:id])
+    if @student.active
+      @student.update(active: false)
+    else
+      @student.update(active: true)
+    end
+
+    redirect_to student_path(@student)
   end
 
   private
