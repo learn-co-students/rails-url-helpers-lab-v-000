a786b88004faf04a0daf21282d6acb923787ef68
lucketta <lucketta@umich.edu>
2017-05-14 22:59:28 -0400

    Done.

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
+      t.boolean :active, default: 0
       t.timestamps null: false
     end
   end
