7d3b13fb1240ecfaa11e35dd10e0e3320e4ccb71
Menachem Hornbacher <me@mendel.tech>
2017-06-06 13:19:51 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def active
+    @student = Student.find(params[:id])
+    @student.update(active: !@student.active)
+    redirect_to student_path(@student)
   end
 
   private
