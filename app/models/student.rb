6975921ec008f30ffc2c4045920ea98141883f56
Samantha Knight <sammay82@yahoo.com>
2017-06-24 03:26:59 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_phrase
+    if active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+  def active_action
+    active ? "Deactivate" : "Activate"
+  end
 end
