df2a390dcbcf4d1e64362a0a296a479c401b0f06
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-06-29 22:54:05 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,10 @@
 class Student < ActiveRecord::Base
+
+  def status
+    self.active ? "This student is currently active." : "This student is currently inactive."
+  end
   def to_s
     self.first_name + " " + self.last_name
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Qasim", last_name: "Hassan")
+ Student.create(first_name: "Hunniya ", last_name: "Hassan", active: true)
