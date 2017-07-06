65714932469bf20953c6a764f4578ba144a3f751
curlywallst <nnoyes@comcast.net>
2017-07-06 14:08:07 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,5 @@
 class Student < ActiveRecord::Base
 
-  def initialize(attributes=nil)
-      attr_with_defaults = {:active => false}.merge(attributes)
-      super(attr_with_defaults)
-  end
 
   def to_s
     self.first_name + " " + self.last_name
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,9 +3,9 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.boolean :active
 
       t.timestamps null: false
     end
   end
+
 end
