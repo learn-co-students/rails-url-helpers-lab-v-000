91fcfd16864b5be2adf93cef0be1339feae68fc2
blinded93 <blinded93@gmail.com>
2017-07-20 22:25:13 -0700

    passed tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+
+    @student.toggle_active
+    redirect_to "/students/#{@student.id}"
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def status
+    if self.active == true
+      "active"
+    else
+      "inactive"
+    end
+  end
+
+  def toggle_active
+    self.toggle!(:active)
+  end
 end
