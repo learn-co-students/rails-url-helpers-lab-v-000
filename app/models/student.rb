bc4a413aff82143c3c10b47dff45f0ee40107a7e
Spencer Evans <spencevans@gmail.com>
2017-08-01 03:05:15 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    self.active = true
+  end
+
+  def deactivate
+    self.active = false
+  end
 end
