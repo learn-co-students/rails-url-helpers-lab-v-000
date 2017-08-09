aea9a6ca532fd182ca6d002d13761b6c9bfc6e01
streamedLine <dovidlwtn@gmail.com>
2017-08-08 22:59:15 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
 
   def activate
     student = Student.find(params[:id])
-    student.active = !student.active 
+    student.update(active: !student.active)
     redirect_to student_path(student.id)
   end
 
