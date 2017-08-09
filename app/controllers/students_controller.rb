45baadee1055aa21672f86ab664e7b9e53b82d3a
Parv Kumar <pkumar@trinity.edu>
2017-08-09 00:53:17 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -8,9 +9,22 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate_student
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.active = true
+    else
+      @student.active = false
+    end
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+
 end
