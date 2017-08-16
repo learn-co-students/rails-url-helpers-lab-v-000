f1e2effac4d07ffd08b94f6c473c903ad69f9e74
gb23 <gregbenj@hotmail.com>
2017-08-16 14:04:50 -0700

    work on activate

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  # before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    show
   end
 
   private
