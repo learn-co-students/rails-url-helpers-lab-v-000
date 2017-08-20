4841c9a71f402eed0eb240de165e563c91ddbde2
Sohee Cho <soheecho94@gmail.com>
2017-08-20 05:22:45 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :show]
   get '/students/:id/activate', to: 'students#activate', as: 'activate/student'
+  #as -> prefix!
 end
