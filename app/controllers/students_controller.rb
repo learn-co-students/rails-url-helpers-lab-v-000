acc256c6f2039a101cbf6dfe91e63134fac169ad
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-09-07 09:30:57 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student=set_student
+
+  end
+
+  def toggle
+   @student=set_student
+   @student.active= !@student.active
+   @student.save
+   redirect_to student_path(@student.id)
   end
 
   private
