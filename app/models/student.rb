d727f38c1f055919d1191ed600c3a6472ccda9bf
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-10-08 16:29:59 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def change_active
+    self.active = !self.active
+    self.save
+  end
+
+  # def active=(val)
+  #   self.active = val
+  # end
 end
