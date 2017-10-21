f368972f09ef6ebec7673562d4851518ad998de7
Scott Wakeman <github email address>
2017-10-20 22:19:40 -0700

    Done.

--- db/migrate/20171021041042_create_students.rb
+++ db/migrate/20171021041042_create_students.rb
@@ -3,8 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
-      t.timestamps null: false
+      t.boolean :active, :default => true
     end
   end
 end
---
+++ db/migrate/20171021041605_change_students.rb
@@ -0,0 +1,5 @@
+class ChangeStudents < ActiveRecord::Migration
+  def change
+    change_column_default :students, :active, false
+  end
+end
