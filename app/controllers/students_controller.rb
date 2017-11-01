dde58929f9bab7df9afbf02f9486d3c5bf13553e
github username <github email address>
2017-10-31 21:06:26 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,17 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
   
-  def index
+  def index # implicitly render views/students/index.html.erb view file, which has the same name as #index method (action)
     @students = Student.all
   end
 
-  def show
+  def show # implicitly render the views/students/show.html.erb view file, which has same name as #show method (action)
+    @student = Student.find(params[:id])
   end
 
-  private
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle!(:active) # @student instance's active attribute takes on the opposite boolean value, i.e., true becomes false / false becomes true. Changes are saved to database.
+    redirect_to student_path(@student)
+  end
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
