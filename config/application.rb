4b9b2d7907cf1f2e7f582f041662818829f7e2a8
Jason Decker <curiositypaths@users.noreply.github.com>
2017-11-01 02:39:40 -0400

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Arlab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.development_only = false
   end
 end
