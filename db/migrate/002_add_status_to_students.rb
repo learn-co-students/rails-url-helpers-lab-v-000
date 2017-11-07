f82ee929babea1f8bb5866a1e4056581a77a5916
Jacqlene Sifre <jsifre01@gmail.com>
2017-11-07 01:28:15 +0000

    Done.

--- db/migrate/002_add_status_to_students.rb
+++ db/migrate/002_add_status_to_students.rb
@@ -1,6 +1,6 @@
 class AddStatusToStudents < ActiveRecord::Migration
 
   def change
-    add_column :students, :active, :boolean, :default => false
+    add_column :students, :active, :boolean, default: false
   end
 end
