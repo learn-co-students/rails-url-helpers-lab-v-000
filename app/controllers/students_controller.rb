cfd7812c154c675a9f92ee8ba5bca585e9b4192a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-11 15:27:24 -0500

    created show and activate page.  still working on updating student's active status

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate, :update]
+
   def index
+    # byebug
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+  end
+
+  def update
+    byebug
+    @student.active = params["student"]["active"]
+    @student.save
+    redirect to student_path(@student)
   end
 
   private
