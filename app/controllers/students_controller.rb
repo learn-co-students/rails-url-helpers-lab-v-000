ec9ef392ba0b2d3a39ddbe8dad207a93ad132713
github username <github email address>
2017-11-21 17:50:25 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student = Student.find(params[:id]) #find student
+    @student.active = !@student.active #reverse activation status
+    @student.save #save changes to db
+    redirect_to student_path(@student) #redirect to student show page
   end
 
   private
