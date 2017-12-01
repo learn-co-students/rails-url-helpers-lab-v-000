f15229038aad5c5a6d287831a67026d9146034a8
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-12-01 17:43:36 +0000

    Done.

---
+++ db/migrate/20171201165133_add_active_boolean.rb
@@ -0,0 +1,5 @@
+class AddActiveBoolean < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, :default => false
+  end
+end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 describe Student do
   before(:each) do
