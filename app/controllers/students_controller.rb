d3922f5b2104265689d9adf7b3a1eb3318833cc7
Olena Ageyeva <eageeva@hotmail.com>
2018-01-01 20:15:43 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,11 +14,12 @@ class StudentsController < ApplicationController
    end
   end
 
-#  def activate
-#    @student.active=!@student.active
-#    @student.save
-#    redirect_to student_path(@student)
-#  end
+  def activate
+    set_student
+    @student.active=!@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 
   private
 
