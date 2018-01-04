ed6f5b51dcde11154380820e598a9a268d539798
EC2 Default User <ec2-user@ip-172-31-46-51.us-west-2.compute.internal>
2018-01-04 06:03:11 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,16 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
   end
+  
+  def activate
+    set_student
+    @student.active = !@student.active 
+    @student.save
+    redirect_to student_path(@student)
+  end
+    
 
   private
 
