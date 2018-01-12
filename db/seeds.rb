0afc005e3278fdbc4aae68f1c4addf30da4594c6
Richard Steinbach <steinbach.dick@gmail.com>
2018-01-12 11:11:42 -0800

    rails-url-helpers-lab-v-000

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create([{ first_name: "Rick", last_name: "Sanchez" }, { first_name: "Morty", last_name: "Smith" }])
