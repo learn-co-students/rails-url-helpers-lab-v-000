71fceeebdc0decac1b767d9fe91c48563b35afb8
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-01-12 11:49:18 -0500

    removed call to previously removed gem web-console as it is breaking with new rails gem

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module Arlab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.web_console.development_only = false
   end
 end
