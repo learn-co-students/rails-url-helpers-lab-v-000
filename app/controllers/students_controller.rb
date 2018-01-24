5bbdd323adda7da16e0eac68aff944a02779a04c
Hannah Carnes <hecarnes@gmail.com>
2018-01-23 19:57:02 -0500

    three tests left

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,17 @@
+require 'pry'
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  #before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+    @status = @student.status
   end
+
+
 
   private
 
