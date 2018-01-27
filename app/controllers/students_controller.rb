4e2d8904aa06efd274bcb37a7531a2a959f97e7d
Chris Boujoukos <github email address>
2018-01-27 13:09:35 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.active = true
+    else
+      @student.active = false
+    end
+    @student.save
+    redirect_to student_path
   end
 
   private
---
+++ db/migrate/002_add_active_column_to_students.rb
@@ -0,0 +1,8 @@
+class AddActiveColumnToStudents < ActiveRecord::Migration
+  def up
+    add_column :students, :active, :boolean, :default => false
+  end
+  def down
+    remove_column :students, :active
+  end
+end
