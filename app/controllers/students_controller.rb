5a08d7803da56f3b28c258d7b1ed6f5cf7037bdd
Brittany Morris <brittmmendez@gmail.com>
2018-02-07 01:49:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,26 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
   end
 
-  private
+  def activate
+   @student = set_student
+   @student.update(active: !@student.active)
+   redirect_to student_path(@student)
+ end
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+ private
+
+   def set_student
+     @student = Student.find(params[:id])
+   end
+
+
+
 end
