1cfae0581f45f5467f43e2b7b583efe88c5d9837
David Towster <djt4xe@virginia.edu>
2018-02-26 19:18:59 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,9 +6,17 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  #private
 
     def set_student
       @student = Student.find(params[:id])
---
+++ db/migrate/002_add_active_col_to_students.rb
@@ -0,0 +1,5 @@
+class AddActiveColToStudents < ActiveRecord::Migration
+    def change
+        add_column :students, :active, :boolean, :default => false
+    end
+  end
