e2282cd987b62cba4e57e0bc9d520092cc61c704
saurookadook <maskiella@gmail.com>
2018-03-01 08:49:33 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
 class Student < ActiveRecord::Base
+  include ActiveModel::AttributeMethods
+
+  define_attribute_methods 'active'
+  before_init :active
+
+  attr_writer :active
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active
+    self.active = false if self.active.nil?
   end
 end
