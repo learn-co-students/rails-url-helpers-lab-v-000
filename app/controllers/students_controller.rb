321a8a8350a7d92da1d69a168ea7c6ebaa507636
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-08 17:26:19 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def active
+    # @student = Student.find(params[:id])
+    # if @student.active
+    #   @student.update(active: false)
+    # else
+    #   @student.update(active: true)
+    # end
   end
 
   private
