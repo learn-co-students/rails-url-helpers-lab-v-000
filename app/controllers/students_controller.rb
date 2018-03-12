cd39ca2d58c700dae8452c01fbb27ddfc16ac129
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 16:55:01 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,7 @@ class StudentsController < ApplicationController
   def activate
     student = Student.find(params[:id])
     student.toggle!(:active)
+    redirect_to student_path(student)
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.boolean  "active",     default: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "active",     default: false
   end
 
 end
