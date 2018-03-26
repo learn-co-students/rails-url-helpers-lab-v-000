560779c37e7def5fe9c243a3a1be89148c5f2cff
Katrina Brinson <kbrinso@gmail.com>
2018-03-26 02:52:09 +0000

    Finished all but active

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,6 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
       t.timestamps null: false
     end
   end
