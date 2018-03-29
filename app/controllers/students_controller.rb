f53900c69d102de3e3c9ea45130007cf855be876
Susan Fowler <infopro94@gmail.com>
2018-03-29 13:47:04 -0500

    updated views with fewer errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
 end
