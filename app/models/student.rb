26c79746ef0a6e7b9bc1e564bda66a92ff9e4c26
Sean Oughton <seanoughton@gmail.com>
2018-04-18 23:48:29 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_status
+    if set_student.active == false
+      @active_status = "inactive"
+    else
+      @active_status = "active"
+    end
+    @active_status
+  end
+  
 end
