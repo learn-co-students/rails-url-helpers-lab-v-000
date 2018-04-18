b081150e37d8a115f1fe500481efe71a43913003
Sean Oughton <seanoughton@gmail.com>
2018-04-18 23:04:28 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 1) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
