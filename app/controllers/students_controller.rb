c4af2f2b227f54ee5671a39fa0df1f718fa0d609
chaimoosh <chaimoosh@gmail.com>
2018-04-23 15:37:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,28 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    if @student.active == false
+      @active = "inactive"
+    else
+      @active = "active"
+    end
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @student.active = true
+      @student.save
+    else
+      @student.active = false
+      @student.save
+    end
+    redirect_to show
   end
 
   private
---
+++ db/migrate/20180423185747_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+      create_table :students do |t|
+        t.string :first_name
+        t.string :last_name
+        t.boolean :active, default: false
+        t.timestamps null: false
+    end
+  end
+end
