6289509b77da809d9c001b02fc9bf834b97f29dd
jokerdelta <jokerdelta@yahoo.com>
2018-04-28 18:59:30 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,6 @@
+
+require 'pry'
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
 
@@ -7,7 +10,7 @@ class StudentsController < ApplicationController
 
   def show
     set_student
-    if @student.active
+    if @student.active == true
       @status = "active"
     else
       @status = "inactive"
@@ -16,13 +19,10 @@ class StudentsController < ApplicationController
   end
 
   def activate
-    set_student
-      if @student.active == "true"
-        @student.active == "false"
-      elsif @student.active == "false"
-        @student.active == "true"
-      end
-    @student.save
+      set_student
+      update_student
+
+    redirect_to @student
   end
 
   private
@@ -30,4 +30,15 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
+    def update_student
+      set_student
+            if @student.active == true
+        @student.active = false
+      else
+        @student.active = true
+      end
+      @student.save
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'students/:id', to: 'students#show', as: 'student'
 
-
+  get 'students/:id/activate', to: 'students#activate', as: 'activate_student'
 
 
 
