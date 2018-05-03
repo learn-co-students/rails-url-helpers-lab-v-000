37de09a3606a47441dcadd368908a46b9697f98a
github username <github email address>
2018-05-03 13:18:46 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,9 @@
+require 'rack-flash'
+
 class StudentsController < ApplicationController
+
+  use Rack::Flash
+
   before_action :set_student, only: :show
   
   def index
@@ -8,9 +13,26 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def active 
+    @student = Student.find(params[:id])
+    if @student.active == true
+      flash[:message] = "This student is currently active."
+    else
+      flash[:message] = "This student is currently inactive."
+    end
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def active 
+      if @student.active == true
+        flash[:message] = "This student is currently active."
+      else
+        flash[:message] = "This student is currently inactive."
+      end
     end
 end
