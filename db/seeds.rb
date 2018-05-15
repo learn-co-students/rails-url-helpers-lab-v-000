9e4c87c73fe7167f8ceb97bf274630447def721f
Kim Gonzales <Kimgo589@gmail.com>
2018-05-15 12:13:26 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name:"Kim", last_name:"Gonzales", active: false)
