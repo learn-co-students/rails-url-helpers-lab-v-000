3b1963a49242bc4a4b84840d764d1181b0f1c5f7
AlexFriedland <friedlandaj@gmail.com>
2018-05-18 11:32:47 +0200

    activate routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
