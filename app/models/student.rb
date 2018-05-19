f00b18535c03b4938c4ae1f9864403d67ab2d321
kristinreddington <kristinreddington@gmail.com>
2018-05-19 02:04:53 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def status
+    if self.active
+      @status = "This student is currently active."
+    else
+      @status = "This student is currently inactive."
+    end
   end
 end
---
+++ db/migrate/20180519053257_add_default_value_to_active_attribute.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToActiveAttribute < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, null: false, default: false 
+  end
+end
