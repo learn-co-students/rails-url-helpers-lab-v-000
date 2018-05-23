a3d4acb9209f94a7f2af6e4478331061e3c9ee72
mstmari <michael.landrum.st.marie@gmail.com>
2018-05-23 10:47:14 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -12,5 +12,13 @@ class StudentsController < ApplicationController
 
     def set_student
       @student = Student.find(params[:id])
+    end
+
+    def activate
+      if @student.active == true
+        false
+      else
+        @student.active = false
+      end
     end
 end
---
+++ db/migrate/20180523140406_add_feature_to_students.rb
@@ -0,0 +1,5 @@
+class AddFeatureToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+end
