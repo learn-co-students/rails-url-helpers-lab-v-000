5817a2276cfd4464192d9560893d73d691b3acba
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-30 21:50:59 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Lauren", last_name: "Perchitti", active: false)
