25b97f55f1f9de2789c0c834832e39024fdce025
Larry <github email address>
2018-06-03 15:28:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,12 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+    @student.update active: !@student.active
+    redirect_to student_path(@student)
+  end
 
   private
 
