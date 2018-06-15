1623ebcbb864faae8349df7abfdbe83b8ea9f0e8
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-15 15:41:40 -0400

    All Tests Passing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
   def show
+    @active_status = @student.active ? 
+      "active" : "inactive"
+  end
+
+  def activate
+    @student.active = !(@student.active)
+    @student.save
+
+    redirect_to action: "show"
   end
 
   private
