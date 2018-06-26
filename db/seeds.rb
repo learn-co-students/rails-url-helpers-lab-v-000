9c352fe69cbca5cb1309c29be6b00533c8961f8b
Nicholas Powers <njpowers7915@gmail.com>
2018-06-26 23:52:07 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+students = Student.create(first_name: 'Nick', last_name: 'Powers')
