8248095ce964f725e7c57c022678fa229f34fa1e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-26 23:06:51 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate_student
+    student = set_student
+    if student.active == true
+      student.update(active: false)
+    else
+      student.update(active: true)
+    end
+    rendor 'students/show'
   end
 
   private
