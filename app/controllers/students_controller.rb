71ededecce862a6cf0e68c3c1c3b30bd11d736c4
Vanessa Toro <vanetoro84@gmail.com>
2018-06-28 18:20:35 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+      @student = Student.find(params[:id])
+  end
+
+  def activate
+      binding.pry
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,10 @@
 class Student < ActiveRecord::Base
+  attr_accessor :active
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def active=(active = false)
+    @active = active
   end
 end
