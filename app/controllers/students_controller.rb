a9c971249ad64824fef233df0aeb222769c00934
Vanessa Toro <vanetoro84@gmail.com>
2018-06-28 20:10:17 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,10 +9,19 @@ class StudentsController < ApplicationController
 
   def show
       @student = Student.find(params[:id])
+      if @student.active == false
+        @status = "This student is currently inactive."
+      else
+        @status = "This student is currently active."
+      end
+
   end
 
   def activate
-      binding.pry
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index]
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get "students/:id/active", to: "students#active", as: 'activate_student'
+
+  get 'students/:id', to: 'students#show', as: 'student'
+  get "students/:id/activate", to: "students#activate", as: 'activate_student'
 
 
 
