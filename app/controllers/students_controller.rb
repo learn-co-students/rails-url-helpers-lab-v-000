5614a0e8974883394be2d2c204b14900a5d19c20
Lucy Wall <lhwall@gmail.com>
2018-07-03 14:17:01 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
+require "pry"
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+    binding.pry
+    if @student.active
+      @status = "active"
+    else
+      @status = "inactive"
+    end
   end
 
   private
