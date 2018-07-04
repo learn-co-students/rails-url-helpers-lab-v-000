8a6f2f6220a660a1d811052bead8a5d9b7aa608c
Zac Willmington <zac.willmington@gmail.com>
2018-07-04 15:37:26 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,26 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+      @student = Student.find(params[:id])
   end
 
+  def activate
+      @student = Student.find(params[:id])
+      if @student.active == true
+          @student.active = false
+      else
+         @student.active = true
+      end
+      @student.save
+      redirect_to student_path(@student)
+  end
   private
 
     def set_student
