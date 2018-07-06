7bd957ab515c9fae94522aa22d07d84680802ab8
Karl Amoguis <ktamoguis@gmail.com>
2018-07-06 09:54:23 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,23 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, :set_status, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
+
   end
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+    def set_status
+      @student.active == false ? @status = "inactive" : @status = "active"
+    end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active
+    @active ||= false
+  end
+
+  def active=(status)
+    @active = status
+  end
+
 end
