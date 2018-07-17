0596da3640e3d9f861b952f3933cadfdbed965a8
Chris Alberti <christopherjalberti@gmail.com>
2018-07-17 14:07:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    student = Student.find(params[:id])
+    if student.active
+      student.status = 0
+      student.save
+    else
+      student.status = 1
+      student.save
+    end
+    redirect_to "/students/#{student.id}"
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active
+    self.status == 1
+  end
+
+  def active=(boolean)
+    if boolean
+      self.status = 1
+    else
+      self.status = 0
+    end
+  end
 end
