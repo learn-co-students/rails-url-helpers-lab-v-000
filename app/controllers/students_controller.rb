2e3e2f7b52565a71674497fea6f92888a3369df4
epic0448 <samuelogundiran@Yahoo.com>
2018-07-24 01:06:14 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+
+  def activate
+    @student = Student.find(params[:id])
+
   end
 
   private
