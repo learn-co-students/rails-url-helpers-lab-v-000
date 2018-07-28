b512a3de7e36b6dcaa51cc53b5d51c2a08f16737
Maverick Tesini <mavericktesini@gmail.com>
2018-07-28 04:25:49 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate #cause toggle of .active attr using a method
+    set_student.activate
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+def activate
+  @student = Student.find(params[:id])
+  @student.active = !@student.active
+  @student.save
+end
+
+
+
 end
