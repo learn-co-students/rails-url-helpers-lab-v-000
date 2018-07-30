e5d03f100afcf77d27063033eee0924beb818010
Carrie Savinell <carrielane89@gmail.com>
2018-07-30 17:56:52 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
+  end
+
+  def activate
+    set_student.active_toggle
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def show
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_toggle
+    self.active = !self.active 
+  end
 end
