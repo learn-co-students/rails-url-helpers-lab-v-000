222754593e5521f7eb90b099029f323ceca5400e
Barbara Washington <barbara.m.washington@gmail.com>
2018-08-07 00:31:31 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,11 @@ class StudentsController < ApplicationController
   end
 
   def activate
-
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
-
 
   private
 
