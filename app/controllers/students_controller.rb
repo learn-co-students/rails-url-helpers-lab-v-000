2aa7a94dfadb3a390754bf9555548e1b91497373
Frank McCallion <fkmccallion@gmail.com>
2018-09-01 17:55:07 +0000

    update to github

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,14 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
 
   private
 
