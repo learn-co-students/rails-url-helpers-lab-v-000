7364b6bfced0ba3656f2371c65b24d9eb66c3c5f
Christine Xiang <yutingcxiang@gmail.com>
2018-09-04 02:08:40 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,20 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def activate
+    @student = Student.find_by(id: params[:id])
+    
+    if @student.active == true
+      @student.active = false
+    else
+      @student.active = true
+    end
+    
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
