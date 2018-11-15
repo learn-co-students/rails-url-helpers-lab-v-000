0531c818f2f821d16c4114adfc60ce81c2055a68
Brian Birdwell <bbirdwell@gmail.com>
2018-11-15 22:14:35 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,29 @@
+require 'pry'
 class StudentsController < ApplicationController
+
+
   before_action :set_student, only: :show
-  
+
+
   def index
     @students = Student.all
   end
 
   def show
+
+  end
+
+  def edit
+    # binding.pry
+    set_student
+    if @student.active == true
+      @student.active = false
+      @student.save
+    else
+      @student.active = true
+      @student.save
+    end
+    redirect_to @student
   end
 
   private
@@ -13,4 +31,7 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ActiveRecord::Base
+  after_create :deactivate
+
+  def deactivate
+    self.active = false
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
