359ad01b7bfff2d01e546f438e893bf5a72be4de
github username <github email address>
2018-11-26 16:20:04 +0000

    Done.

--- db/migrate/20181126153550_add_active_to_students.rb
+++
@@ -1,5 +0,0 @@
-class AddActiveToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :active, :boolean, default: false
-  end
-end
--- db/migrate/20181126160035_change_column_default.rb
+++
@@ -1,4 +0,0 @@
-class ChangeColumnDefault < ActiveRecord::Migration
-  def change
-  end
-end
