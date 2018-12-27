ae269b31d308712afa24a107af90569d328c089f
Tammy Metz <tammy@tammymetz.com>
2018-12-27 19:07:00 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,26 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  #before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def update
+    @student = Student.find_by_id(params[:id])
+    #@student.toggle!(:active)
+    #@student.update(active: !@student.active?)
+    if @student.active?
+      puts "switching to false"
+      @student.update(active: false)
+    else
+      puts "switching to true"
+      @student.update(active: true)
+    end
+    redirect_to student_path(@student)
   end
 
   private
