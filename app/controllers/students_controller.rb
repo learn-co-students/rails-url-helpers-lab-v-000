28753d11e70af75774d069744e97ce9ae2bf771d
Judy McGoldrick <jjeanius@verizon.net>
2018-12-28 06:09:12 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
 
@@ -6,31 +7,24 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
-     if params[:status] == "active"
-          render :action => "show"
-        else
-          render :action => "show"
-        end
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    #   if params[:status] == "active"
-    #     @student.active = true
-    #     @student.save
-  #      @students = Student.inactive
-  #      render :action => "show"
-  #    else
-  #      @students = Student.active
-  #      render :action => "show"
+  def set_student
+  #  binding.pry
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active == true
+      @student.active = false
+    else
+        @student.active= true
     end
-
-    def activate
-
-    end
-
+    @student.save
+    redirect_to @student
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active?
+    if self.active == false
+      "This student is currently inactive."
+    else
+      "This student is currently active."
+    end
+  end
+
 end
