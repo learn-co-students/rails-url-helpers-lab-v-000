9e74637820ca99f2a869649799225798706eadf5
Justine B <justinegayton@gmail.com>
2019-01-23 19:54:32 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,29 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
+  def new 
+    @student = Student.new 
+  end 
+  
+  def create
+  end 
+  
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+  
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle :active
+    @student.save
+    redirect_to "/students/#{@student.id}"
   end
 
   private
-
     def set_student
       @student = Student.find(params[:id])
     end
---
+++ db/migrate/20181214172836_add_active_status.rb
@@ -0,0 +1,5 @@
+class AddActiveStatus < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, null: false, default: false
+  end
+end
