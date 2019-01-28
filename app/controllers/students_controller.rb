1bd2dfb34c3bb44b075f3ee6b8e35a09c46026af
Sushi Chanrai <sushi@pathworks.co>
2019-01-28 12:16:17 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
+
   def index
     @students = Student.all
   end
 
   def show
+
+  end
+
+  def activate
+      @student.active = !@student.active
+      @student.save
+      redirect_to student_path(@student)
+      #route helper - it references the show path, needs the id or instance as part of url
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :show]
+  #resources will append and map all the standard restful routes like new,index create to the uri you specify
+  #in resources: Above though is only mapping students/index and students/show
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get "students/:id", to: "students#show"
+  #get "students/:id", to: "students#show"
+  get "students/:id/activate", to: "students#activate", as: 'activate_student'
+
 end
