16ac1c0c6200b84e5ae9b77bd841fd976b757eda
Erin Green <erin.y.green@gmail.com>
2019-01-31 20:20:37 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,13 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
