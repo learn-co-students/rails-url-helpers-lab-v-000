2e7bb2504f596aaf5b92426d8b9580751968e442
Rachel Wyatt <rw126@evansville.edu>
2019-02-13 21:05:46 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,21 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def student_path
+    @students = Student.all
+  end
+  
+  def activate_student_path
+    @students = Student.all 
+  end
+  
+  def active? 
+    if self.active == true 
+      "active" 
+    elsif self.active == false
+      "inactive"
+    end
+    
+  end
 end
