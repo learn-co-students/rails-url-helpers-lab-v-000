1e6f9e56ac02eb7427511e9dc8a70ac0b0c4f96f
lpzraf <lpz.raf@gmail.com>
2019-02-17 18:28:44 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,17 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
   end
+
+  def active
+    @students.active
+  end
+
 
   private
 
