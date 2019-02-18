aa46a06678c4e2e5ad6dd588a98edd98e505007d
lpzraf <lpz.raf@gmail.com>
2019-02-17 19:10:48 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
 
   def index
     @students = Student.all
@@ -8,8 +8,10 @@ class StudentsController < ApplicationController
   def show
   end
 
-  def active
-    @students.active
+  def activate
+   @student.active = !@student.active
+   @student.save
+   redirect_to student_path(@student)
   end
 
 
