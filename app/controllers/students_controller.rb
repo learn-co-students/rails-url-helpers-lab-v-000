e9c2baea3ff2f270a68774f1dd000fbf83e742c7
brennanmrose <brennan.m.rose@gmail.com>
2019-03-11 22:15:15 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,15 @@ class StudentsController < ApplicationController
   def show
   end
 
-  def activate_student_path
+  def activate
+    set_student
+    if @student.active == true 
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    redirect_to action: 'show'
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -9,4 +9,5 @@ class Student < ActiveRecord::Base
   def activated?
   	!!(self.active)
   end
+
 end
