5ca6a15180263870d2a4186e4e0b6771bc145e93
brennanmrose <brennan.m.rose@gmail.com>
2019-03-11 22:18:31 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
       @student.active = true
     end
     @student.save
-    redirect_to action: 'show'
+    redirect_to student_path(@student)
   end
 
   private
