cfeb36b57bd43408c45964ac188a319ca03d5637
Samantha brake <sammy.brake@gmail.com>
2019-04-02 15:09:41 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
   end
 
+  def activate_student
+    set_student
+  end
+
+  patch '/tweets/:id' do
+  end
   private
 
     def set_student
