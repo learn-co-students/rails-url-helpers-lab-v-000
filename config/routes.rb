51730830970f950e33c4ad4f0c6de70e537fa418
github username <github email address>
2019-06-07 20:19:12 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+  get '/activate', to: 'user#activate', as: 'activate'
 end
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,9 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
+      #Create a new boolean attribute called active in the students table.
+      t.boolean :active, :default =>  false
+      #Ex:- :default =>''
       t.timestamps null: false
     end
   end
