22a1635e07daf787af27ed1dd5d13e072ea3e162
github username <github email address>
2019-07-20 11:56:25 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,18 +1,20 @@
 class Student < ActiveRecord::Base
+  attr_accessor :active 
   def to_s
     self.first_name + " " + self.last_name
   end 
 
-  def activate(Student.active, active)
-    Student.activate.helper   #DRY
-    if @student = @student && Student(params[:id])
-      @student.true == true
-    else !@student = !@student 
-      !@student.false == false
-      redirect_to: '/student'
-    elsif
-      @student == Student.de-activate && Student.delete.all
-        !@student 
-        end
-      end
+
+  # def activate
+  #   Student.activate.helper   #DRY
+  #   if @student = @student && Student(params[:id])
+  #     @student.true == true
+  #   else !@student = !@student 
+  #     !@student.false == false
+  #     redirect_to: '/student'
+  #   elsif
+  #     @student == Student.de-activate && Student.delete.all
+  #       !@student 
+  #       end
+  #     end
 end
