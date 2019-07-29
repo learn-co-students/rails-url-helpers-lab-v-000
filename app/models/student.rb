f06b3e0633f1916787de25111c150af1ff84246d
github username <github email address>
2019-07-29 11:42:08 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  attr_accessor :active
+  # attr_accessor :active
   
   def to_s
     self.first_name + " " + self.last_name
--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -46,6 +46,7 @@ describe 'Show page' do
   it 'renders the active status if the user is active' do
     @student.active = true
     @student.save
+    
     visit student_path(@student)
     expect(page).to have_content("This student is currently active.")
   end
