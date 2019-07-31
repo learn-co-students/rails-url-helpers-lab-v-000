bac95e92024fe3b5b3a1ac201984027b09ce1dbe
Thomas Gray <thomas.gray@tgray.local>
2019-07-31 09:52:51 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_status
+    if self.active == true
+      "active"
+    else
+      "inactive"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:index, :show]
+  resources :activate_student, only: [:activate]
+
+  get 'students/:id/activate', to: 'students#activate', as: 'activate_student'
+
+  patch '/students/:id', to: 'students#update'
 end
