23f0096763d6d0366705e6ffe6ce231b3824fdeb
github username <github email address>
2019-10-24 12:27:11 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,10 +15,11 @@ class StudentsController < ApplicationController
     @student.save
     redirect_to student_path(@student)
   end
-  
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+  
 end
