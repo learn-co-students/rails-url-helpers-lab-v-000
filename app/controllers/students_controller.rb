46c176d83c3ca63d5adc3428d0a91bdad628da65
Amee Covarrubias <ameecova@yahoo.com>
2019-11-11 22:39:26 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,19 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active 
+      @student.active = false 
+    else 
+      @student.active = true 
+    end 
+    @student.active
+    @student.save 
+    
+    redirect_to student_path(@student)
+  end 
 
   private
 
