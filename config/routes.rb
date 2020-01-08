d877e8013ac35e8c08682be9acc36a9266b4802f
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-08 19:47:39 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :show]
+# verb    URI Pattern(Path)    Controller#action        names the Helper path prefix
   get 'students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
