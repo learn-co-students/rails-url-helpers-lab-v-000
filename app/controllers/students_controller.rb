bdaa66a2cd814418b9b44cea8bcbdd3d90e8d173
github username <github email address>
2020-03-09 14:49:16 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,17 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+
 end
