5ef5f9b28a8d2f1cb0c813a529fff58b9195178b
parisdolfman <parisdolfman@gmail.com>
2021-03-19 13:08:24 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Paris", last_name: "Dolfman", active: true)
+
+puts "data loaded success"
