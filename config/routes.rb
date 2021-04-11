89a9d8dc74d1b63e4e7ca7180e2ac18b1094685e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-11 14:36:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # I tried creating 2 individual routes but when I ran rails routes, the show route didn't have a prefix.
+  # By changing to using the resources method, I was able to get the proper singular prefix for the show
+  # route.
+  resources :students, only: [:index, :show]
+  # I was close when I poked at the solution to confirm. It's OK to have a non-CRUD path!! 
+  # The key was to look at the test and see that is was expecting activate_student to find the
+  # _path on. But I was hung up on using edit which wasn't necessary. Lesson learned.
+  # Also, I tried to guess instead of looking at the instructions - 
+  # whoopsie!
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
